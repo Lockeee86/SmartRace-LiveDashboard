af5d65f2f275b5ffa12b1159f07fb2492589234e
Lockeee86 <lockeee86@googlemail.com>
2025-08-06 14:01:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def smartrace_endpoint():
         return jsonify({'error': str(e)}), 400
 
 # API für Live-Daten
-@app.route('/api/live-timing')
+@app.route('/api/live-feed')
 def live_data():
     latest_laps = db.session.query(LapTime).order_by(LapTime.timestamp.desc()).limit(60).all()
     
